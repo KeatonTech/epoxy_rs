f1cdeb4e8eee8ee6dd5ff20a2278001b45764948
Keaton Brandt <keaton.brandt@gmail.com>
2019-06-22 14:45:35 -0700

    Allows nested collections in epoxy_collections

--- epoxy_collections/examples/hash_map.rs
+++ epoxy_collections/examples/hash_map.rs
@@ -1,7 +1,7 @@
 extern crate epoxy_collections;
 extern crate epoxy_streams;
 
-use epoxy_collections::{ReactiveCollection, ReadonlyReactiveCollection, ReactiveHashMap};
+use epoxy_collections::{ReactiveCollection, ReactiveHashMap, ReadonlyReactiveCollection};
 use epoxy_streams::ReactiveCache;
 
 fn main() {
@@ -12,4 +12,12 @@ fn main() {
     hash_map.insert(2, 4);
     assert_eq!(mutations.get().len(), 2);
     assert_eq!(*hash_map.get(&1).unwrap(), 2);
+
+    let nested_hash_map: ReactiveHashMap<i8, ReactiveHashMap<i8, i8>> = ReactiveHashMap::new();
+    nested_hash_map.insert(1, hash_map);
+    let nested_mutations = ReactiveCache::from_stream(nested_hash_map.get_mutation_stream());
+    assert_eq!(nested_mutations.get().len(), 0);
+
+    nested_hash_map.get(&1).unwrap().insert(3, 6);
+    assert_eq!(nested_mutations.get().len(), 1);
 }
--- epoxy_collections/src/reactive_hash_map.rs
+++ epoxy_collections/src/reactive_hash_map.rs
@@ -2,15 +2,14 @@ use super::base_collection::{
     BaseReactiveCollection, ReactiveCollectionInternal, ReadonlyReactiveCollectionInternal,
 };
 use super::mutations::Mutation::{Property, Subproperty};
-use super::mutations::{Mutation, PropertyMutation};
+use super::mutations::{Mutation, PropertyMutation, SubpropertyMutation};
 use super::reactive_container_item::ReactiveContainerItem;
+use std::any::Any;
 use std::cmp::Eq;
 use std::collections::HashMap;
 use std::hash::Hash;
 use std::iter::once;
 use std::sync::{Arc, RwLock};
-use std::any::Any;
-
 
 // READONLY REACTIVE HASH MAP
 
@@ -68,10 +67,7 @@ where
     ValueType: Send,
     ValueType: 'static,
 {
-    pub fn get(
-        &self,
-        key: &KeyType,
-    ) -> Option<Arc<ValueType>> {
+    pub fn get(&self, key: &KeyType) -> Option<Arc<ValueType>> {
         let read_guard = self.base.collection.read().unwrap();
         if let Some(value) = read_guard.get(key) {
             Some(value.clone())
@@ -80,7 +76,6 @@ where
         }
     }
 }
-
 
 // WRITABLE REACTIVE HASH MAP
 
@@ -97,11 +92,11 @@ where
     ValueType: Send,
     ValueType: 'static,
 {
-    map: Arc<ReadonlyReactiveHashMap<KeyType, ValueType>>
+    map: Arc<ReadonlyReactiveHashMap<KeyType, ValueType>>,
+    subproperty_subscriptions: RwLock<HashMap<KeyType, epoxy_streams::Subscription<Mutation>>>,
 }
 
-impl<KeyType, ValueType> ReadonlyReactiveCollectionInternal
-    for ReactiveHashMap<KeyType, ValueType>
+impl<KeyType, ValueType> ReadonlyReactiveCollectionInternal for ReactiveHashMap<KeyType, ValueType>
 where
     KeyType: Hash,
     KeyType: Eq,
@@ -125,8 +120,7 @@ where
     }
 }
 
-impl<KeyType, ValueType> ReactiveCollectionInternal
-    for ReactiveHashMap<KeyType, ValueType>
+impl<KeyType, ValueType> ReactiveCollectionInternal for ReactiveHashMap<KeyType, ValueType>
 where
     KeyType: Hash,
     KeyType: Eq,
@@ -139,15 +133,38 @@ where
     ValueType: Send,
     ValueType: 'static,
 {
-    fn apply_mutation(&self, collection: &mut HashMap<KeyType, Arc<ValueType>>, mutation: &Mutation) {
+    fn apply_mutation(
+        &self,
+        collection: &mut HashMap<KeyType, Arc<ValueType>>,
+        mutation: &Mutation,
+    ) {
         match mutation {
             Property(mutation) => {
                 let key = mutation.key.downcast_ref::<KeyType>().unwrap().clone();
                 if let Some(value_box) = &mutation.new_value {
                     let local_value = value_box.clone();
                     let value = local_value.downcast::<ValueType>().unwrap();
+
+                    if let Some(stream) = value.get_mutation_stream() {
+                        let stream_key = key.clone();
+                        let subscription = stream
+                            .map_rc(move |mutation| {
+                                Arc::new(Mutation::Subproperty(SubpropertyMutation {
+                                    key: Box::new(stream_key.clone()),
+                                    mutation: mutation.clone(),
+                                }))
+                            })
+                            .pipe_into(self.get_mutation_sink());
+
+                        self.subproperty_subscriptions
+                            .write()
+                            .unwrap()
+                            .insert(key.clone(), subscription);
+                    }
+
                     collection.insert(key, value);
                 } else {
+                    self.subproperty_subscriptions.write().unwrap().remove(&key);
                     collection.remove(&key);
                 }
             }
@@ -179,24 +196,19 @@ where
         ReactiveHashMap {
             map: Arc::new(ReadonlyReactiveHashMap {
                 base: BaseReactiveCollection::new(HashMap::new()),
-            })
+            }),
+            subproperty_subscriptions: RwLock::new(HashMap::new()),
         }
     }
 
-    pub fn get(
-        &self,
-        key: &KeyType,
-    ) -> Option<Arc<ValueType>> {
+    pub fn get(&self, key: &KeyType) -> Option<Arc<ValueType>> {
         self.map.get(key)
     }
 
-    fn get_dynamic(
-        &self,
-        key: &KeyType,
-    ) -> Option<Arc<dyn Any + Send + Sync>> {
+    fn get_dynamic(&self, key: &KeyType) -> Option<Arc<dyn Any + Send + Sync>> {
         match self.get(key) {
             Some(arc) => Some(arc),
-            None => None
+            None => None,
         }
     }
 
@@ -206,10 +218,12 @@ where
 
     pub fn insert_rc(&self, key: KeyType, value: Arc<ValueType>) {
         let old_value = self.get_dynamic(&key);
-        self.write_mutations(Box::new(once(Arc::new(Mutation::Property(PropertyMutation {
-            key: Box::new(key),
-            old_value: old_value,
-            new_value: Some(value)
-        })))));
+        self.write_mutations(Box::new(once(Arc::new(Mutation::Property(
+            PropertyMutation {
+                key: Box::new(key),
+                old_value: old_value,
+                new_value: Some(value),
+            },
+        )))));
     }
 }
