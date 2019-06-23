987715eff7341e81ab392e5a330611f9c491c4f0
Keaton Brandt <keaton.brandt@gmail.com>
2019-06-23 13:26:36 -0700

    Fix ReadonlyReactiveHashMap export

--- epoxy_collections/src/lib.rs
+++ epoxy_collections/src/lib.rs
@@ -6,4 +6,5 @@ mod reactive_hash_map;
 pub use base_collection::{ReactiveCollection, ReadonlyReactiveCollection};
 pub use mutations::*;
 pub use reactive_container_item::ReactiveContainerItem;
-pub use reactive_hash_map::reactive_hash_map::{ReactiveHashMap, ReadonlyReactiveHashMap};
+pub use reactive_hash_map::reactive_hash_map::ReactiveHashMap;
+pub use reactive_hash_map::readonly_reactive_hash_map::ReadonlyReactiveHashMap;
